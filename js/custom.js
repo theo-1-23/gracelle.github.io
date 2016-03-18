cbfd9c467c0a07dcead7ca872330cab56510cbab
gracelle <gracellemesina@gmail.com>
2016-03-17 20:58:10 -0700

    Test Chuddy's code

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,23 @@
+$(document).ready(function() {
+
+	/*
+	* Since you are using the same functions, we could combine them into an array and call one function to fire the event
+	*/
+	var array_names = [ 'gradus', 'nove', 'go2gether', 'vsma', 'engage', 'fortune', 'logo', 'cards', 'misc', 'selfie' ];
+
+	/*
+	* Loop through with the names and for each element,  on hover, change image
+	*/
+	for (var i = 0; i < array_names.length; i++) {
+		$('#thumb-' + array_names[i]).on('mouseenter mouseleave', function() { 
+			for (var j = 0; j < array_names.length; j++) {
+				$(this).parent().find('.tags-' + array_names[j]).slideToggle(500);
+			}
+		});
+	} 
+});
+
+/* 
 
 $( document ).ready(function() {
 
@@ -72,3 +92,5 @@ $( document ).ready(function() {
 	}); 	
 
 });
+
+*/
